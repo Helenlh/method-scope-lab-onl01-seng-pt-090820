00a8205e8fb88ef24fbf2f0fb780951fe6be4d54
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-11 17:14:42 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,8 +1,8 @@
 def rescue_princess_peach
   puts "Hooray! Mario has rescued Princess Peach."
- status = "rescued"end 
+ end 
 
 # Comment the below back in to see method scope in action!
 
-# rescue_princess_peach
-# puts status
+rescue_princess_peach
+puts status
